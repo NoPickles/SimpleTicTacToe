ccd547c6264b75762d83d904ed32767105e0b82b
Matthew L Torres <torresmatt20@gmail.com>
2016-10-09 02:12:50 -0500

    stage all

---
+++ script.js
@@ -0,0 +1,24 @@
+function draw(){
+   var canvas = document.getElementById('canvas');
+   if (canvas.getContext){
+     var ctx = canvas.getContext('2d');
+     
+     ctx.strokeStyle="#FF0000";
+     ctx.strokeRect(0,0,300,300);
+
+     
+     ctx.fillStyle = "rgb(200,0,0)";
+     ctx.fillRect (100, 0, 5, 300);
+     
+     ctx.fillStyle = "rgb(200,0,0)";
+     ctx.fillRect (200, 0, 5, 300);
+     
+     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+     ctx.fillRect (0, 100, 300, 5);
+     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+     ctx.fillRect (0, 200, 300, 5);
+          
+     
+     
+   }
+ }
